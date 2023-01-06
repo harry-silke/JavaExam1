8083ef2e299710964677ee4709874bc9a2323115
Harry Silke <u7308974@anu.edu.au>
2023-01-06 14:32:15 +1100

    Initial commit

---
+++ src/ATM.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Bank {
+
+//map for
+
+    Map<Integer, User> accounts;
+
+    public Bank() {
+         accounts = new HashMap<>();
+    }
+
+
+    public addUser(){
+        
+    }
+
+
+}
