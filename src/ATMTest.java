738090c745401a289416ccd146d8d64689b084e8
Harry Silke <u7308974@anu.edu.au>
2023-01-06 14:53:55 +1100

    Updated tests

--- src/ATMTest.java
+++ src/ATMTest.java
@@ -15,13 +15,15 @@ class ATMTest {
         assertTrue(atm.users.get("Smith").getLastname() == "Smith");
         assertTrue(atm.users.get("0403715629").getMobileNumber() == "0403715629");
 
-
-
         assertTrue(atm.users.get(002).getID()==002);
-        
+        assertTrue(atm.users.get("Leanne").getFirstname() == "Leanne");
+        assertTrue(atm.users.get("Smith").getLastname() == "Smith");
+        assertTrue(atm.users.get("0403185031").getMobileNumber() == "0403185031");
 
         assertTrue(atm.users.get(003).getID()==003);
-
+        assertTrue(atm.users.get("Kim").getFirstname() == "Kim");
+        assertTrue(atm.users.get("Kash").getLastname() == "Kash");
+        assertTrue(atm.users.get("0404993021").getMobileNumber() == "0404993021");
 
 
 
