e6594e5e33240e7d31d9c151a61c79bc95f50dd1
Harry Silke <u7308974@anu.edu.au>
2023-01-06 15:54:45 +1100

    Updated Pseudo code

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-
+// loads data and function from ATM
         ATM atm = new ATM();
         atm.loadUsers("data/users.txt");
 //        System.out.println(atm.loadUsers("users.txt"));
